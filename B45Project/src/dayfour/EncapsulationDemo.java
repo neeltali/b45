30de39c45ff9610a976ec411b68be1a4e7891377
Windows 10 <Windows 10@DESKTOP-OE9L8AE>
2023-02-27 19:07:57 +0530

    commit

---
+++ B45Project/src/dayfour/EncapsulationDemo.java
@@ -0,0 +1,20 @@
+package dayfour;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// object creation
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ B45Project/src/dayfourleran/ht.java
@@ -0,0 +1,16 @@
+package dayfourleran;
+
+import java.util.Scanner;
+
+public class ht {
+
+	public static void main(String[] args) {
+		Scanner ob=new Scanner(System.in);
+		String name;
+		System.out.println("Enter Person Name : ");
+		name = ob.next();
+		
+
+	}
+
+}
